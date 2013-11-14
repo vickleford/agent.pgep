d60ce9e3749bf7ab0d58211eba34052ce3e02b80
Vickleford <vic.watkins@rackspace.com>
2013-11-14 12:25:47 -0600

    objectified the postgres work and sorted out the docs

--- README.md
+++ README.md
@@ -1,6 +1,63 @@
 Rackspace Cloud Monitoring plugin for PostgreSQL endpoints as an 
 agent.plugin type of check.
 
-Log in, select 1, and exit
+Log in, select 1, and exit.
 
-metric status string value
+### Installation
+
+    pip install agent.pgep
+
+### Configuration
+
+Create an INI file in `/etc/agent.pgep.ini`. **Please protect this file's permissions.** I recommend mode 0600 owned by uid 0 and gid 0.
+
+You should title each INI section after the database you're connecting to. You can have multiple databases defined, but you can only test a connection to one at a time. For example, we can have a single database configured 
+
+    # /etc/agent.pgep.ini
+    [my_app_backend]
+    user=my_app_user
+    password=Dk292fdjoidslkkKkls
+    host=db.myapp.com
+    database=my_app_db
+    
+You can put anything in the INI sections that you see in the "basic connection parameters" at http://initd.org/psycopg/docs/module.html#psycopg2.connect : `database`, `user`, `password`, `host`, `port`.
+
+If we were to invoke the plugin on the server, it would run as `agent.pgep my_app_backend`
+    
+Thus, when creating the check in the Rackspace monitoring API, that would translate to a JSON payload roughly equal to:
+
+	POST /entities/<entityId>/checks
+    {
+        'type': 'agent.plugin',
+        'details': {
+            'file': 'agent.pgep',
+            'args': [ 'my_app_backend' ],
+            'timeout': 5000
+        }
+        'label': 'Webserver to PostgreSQL'
+    }
+    
+If your app makes connections to multiple PostgreSQL hosts, you could simply repeat this for for each host it connects to.
+
+### Metrics
+
+This plugin yields the following metrics:
+
+| Metric | Description | Type |
+| ------ | ----------- | ---- |
+| select_one | The result of `SELECT 1;` | int32 |
+| tt_connect | The time (ms) it took to connect | int32 |
+| tt_complete | The total time (ms) elapsed to run | int32 |
+
+### Alarm Example
+
+Here's an example of what your alarm criteria should look like. select_one should always equal 1, so the understanding is 
+general database failure that you shouldn't need to write criteria for. It's included here to clarify what select_one as a
+metric really is.
+
+    if (metric['select_one'] != 1) {
+        return new AlarmStatus(CRITICAL, "Something is VERY wrong");
+    }
+    if (metric['tt_connect'] > 3000) {
+        return new AlarmStatus(WARNING, "DB connection time took #{tt_connect} ms");
+    }
--- check.py
+++ check.py
@@ -23,13 +23,57 @@ import psycopg2
 from sys import exit
 from configobj import ConfigObj
 from os.path import isfile
+from datetime import datetime
 
 
-def die(why):
-    '''Tell what happened then bail out.'''
+class PostgresEndpoint(object):
+    '''Represent a postgres endpoint that we can connect to and 
+    run a health check against.
     
-    print("status {0}".format(str(why).strip('\n')))
-    exit(1)
+    psycopg2 follows PEP 249 so kwargs can be seen in the footnotes at
+    http://www.python.org/dev/peps/pep-0249/#id40
+
+    Also see basic connection parameters (kwargs here) defined at:
+    http://initd.org/psycopg/docs/module.html#psycopg2.connect
+
+    (These constitute what you can put in the config file.)
+    '''
+    
+    def __init__(self, **db_params):
+        self.connection = None
+        self.cursor = None
+        self.start_time = None
+        self.tt_connect = None
+        self.tt_complete = None
+        self.db_params = db_params
+        
+    def __enter__(self):
+        self.start_time = datetime.now()
+        self.connection = psycopg2.connect(**self.db_params)
+        self.tt_connect = datetime.now()
+        self.cursor = self.connection.cursor()
+        return self
+        
+    def __exit__(self, type, value, traceback):
+        self.cursor.close()
+        self.connection.close()
+
+    def select_one(self):
+        self.cursor.execute('SELECT 1;')
+        result = self.cursor.fetchone()
+        self.tt_complete = datetime.now()
+        
+        return result[0]
+        
+    def get_connection_time(self):
+        td = self.tt_connect - self.start_time
+        
+        return td.microseconds / 100
+        
+    def get_complete_time(self):
+        td = self.tt_complete - self.start_time
+        
+        return td.microseconds / 100
 
 
 def get_config():
@@ -47,42 +91,25 @@ def get_config():
     return config
 
 
-def select_one(**kwargs):
-    '''Return what you get when you select 1.
-    
-    psycopg2 follows PEP 249 so kwargs can be seen in the footnotes at
-    http://www.python.org/dev/peps/pep-0249/#id40
-    
-    Also see basic connection parameters (kwargs here) defined at:
-    http://initd.org/psycopg/docs/module.html#psycopg2.connect
-    
-    (These constitute what you can put in the config file.)
-    '''
-    
-    try:
-        conn = psycopg2.connect(**kwargs)
-        cur = conn.cursor()
-        cur.execute("select 1;")
-        result = cur.fetchone()
-    except Exception as e:
-        die(e)
-    finally:
-        cur.close()
-        conn.close()
-        
-    return result[0]
-
-
 def spawn():
     parser = argparse.ArgumentParser()
     parser.add_argument('profile', 
                         help='Profile name from config to use for connection')
     args = parser.parse_args()
+    
     config = get_config()
     
-    result = select_one(**config[args.profile])
+    try:
+        with PostgresEndpoint(**config[args.profile]) as pgep:
+            result = pgep.select_one()
+            tt_connect = pgep.get_connection_time()
+            tt_complete = pgep.get_complete_time()
     
-    print("status OK")
-    print("metric select_one int32 {0}".format(result))
-    # add metrics for time to connect 
-    # add metrics for time to complete
+        print("status OK")
+        print("metric select_one int32 {0}".format(result))
+        print("metric tt_connect int32 {0}".format(tt_connect))
+        print("metric tt_complete int32 {0}".format(tt_complete))
+    except Exception as e:
+        msg = str(e).replace('\n', ' ')
+        print("status {0}".format(msg))
+        exit(1)
